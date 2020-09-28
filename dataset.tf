2a0f1049511fdd3670d4b857d00209a6a996022d
Ok_Lulu <46606776+LuluM84@users.noreply.github.com>
2020-09-28 17:18:49 +0200

    Create dataset.tf

---
+++ dataset.tf
@@ -0,0 +1,15 @@
+# module "bigquery_iam_dataset__<my_dataset>" {
+#   source = ".//../../modules/bigquery_iam_dataset"
+
+#   dataset_id = "<my_dataset>"
+#   project_id = local.project_id
+
+#   viewers = [
+#     "group:${data.terraform_remote_state.common.outputs.groups["<my_group_id>"]}",
+#     "user:<user_email>"
+#   ]
+
+#   editors = [
+
+#   ]
+# }
